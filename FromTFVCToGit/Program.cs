73edb3be94bb39d7dfa3e2fa0c78027a8b01e239
Tobias Waggoner <tobias.waggoner.ext@siemens-healthineers.com>
2023-02-16 15:02:38 +0100

    feat: Something

--- FromTFVCToGit/Program.cs
+++ FromTFVCToGit/Program.cs
@@ -6,7 +6,7 @@ namespace FromTFVCToGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Great change!");
+            Console.WriteLine("Hello World! Great change!!");
         }
     }
 }
